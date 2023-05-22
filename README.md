1f683490aaa7ad665ebf9f43d35f7694b819190a
pankajsethi1 <sethipankaj@ad.corp.google.com>
2023-05-22 10:42:07 -0500

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # sqlalchemy-challenge
+
+* climate_starter.ipnyb
+
+    *   Precipitation Analysis - Queries and plots date and precipitation values for previous 12 months of data
+    *   Station Analysis
+        *   Shows total # of stations
+        *   Queries max,min, and avg temperatures for the most active3 station
+        *   Queries and plots the previous 12 months of TOBS data for the most active station
+
+
+* app.py
+
+    *   Create a Flas application with the following routes:
+        *   A precipitation route that:
+            *   Returns json with the date as the key and the value as the precipitation
+            *   Only returns the jsonified precipitation data for the last year in the database
+        *   A stations route that:
+            *   Returns jsonified data of all of the stations in the database
+        *   A tobs route that:
+            *   Returns jsonified data for the most active station
+            *   Only returns the jsonified data for the last year of data
+        *   A start route that:
+            *   Accepts the start date as a parameter from the URL
+            *   Returns the min, max, and average temperatures calculated from the given start date to the end of the dataset
+        *   A start/end route that:
+            *   Accepts the start and end dates as parameters from the URL
+            *   Returns the min, max, and average temperatures calculated from the given start date to the given end date
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,9 +1,10 @@
 # Import the dependencies.
-from flask import Flask
+from flask import Flask, jsonify
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
+import datetime as dt
 
 
 #################################################
@@ -36,20 +37,110 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+# List all available api routes..
 @app.route("/")
 def home():
-    """List all available api routes."""
     return (
         f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/passengers<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<end>"
+        f"/api/v1.0/start/<start><br/>"
+        f"/api/v1.0/start/end/<start>/<end>"
     )
+
+
+# Display last 12 months of precipitation data when user hits the /api/v1.0/precipitation route
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Calculate the date one year from the last date in data set.
+    date_one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    results=session.query(Measurement.date,Measurement.prcp).\
+        filter(Measurement.date >= date_one_year_ago).all()
+    session.close()
+
+    # Create a dictionary from the results data and append to a list of prec_data
+    prec_data = []
+    for date, prec in results:
+        prec_dict = {}
+        prec_dict["date"] = date
+        prec_dict["prec"] = prec
+        prec_data.append(prec_dict)
+
+    return jsonify(prec_data)
+
+# Display list of stations when the user hits the /api/v1.0/stations route
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Query the stations in the dataset
+    results=session.query(Station.id,Station.name).all()
+    session.close()
+    # Create a dictionary from the results data and append to a list of station_data
+    station_data = []
+    for id,name in results:
+        station_dict = {}
+        station_dict["id"] = id
+        station_dict["name"] = name
+        station_data.append(station_dict)
+
+    return jsonify(station_data)
+
+# Display the dates and temperature observations of the most-active station for the previous year of data
+# when the user hits the /api/v1.0/tobs route
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Find the most active station
+    most_active_station = session.query(Measurement.station,func.count(Measurement.station))\
+        .group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()[0][0]
+    # Query date and precipitation data for the most active station only for last year
+    results=session.query(Measurement.date,Measurement.prcp).filter(Measurement.station == most_active_station)\
+        .filter(Measurement.date >= dt.date(2017,8,23) - dt.timedelta(days=365)).all()
+    session.close()
+    # Create a dictionary from the results data and append to a list of prec_data
+    prec_data = []
+    for date, prec in results:
+        prec_dict = {}
+        prec_dict["date"] = date
+        prec_dict["prec"] = prec
+        prec_data.append(prec_dict)
+
+    return jsonify(prec_data)
+
+# Display min,avg and max for date>=start when the user hits the /api/v1.0/start/<start> route
+
+@app.route("/api/v1.0/start/<start>")
+def start_date_temp(start):
+    # Find min,avg and max for date>=start
+    results=session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >=start).all()
+    session.close()
+    # Create a list of dictionary from the results data
+    temp_data =[{"min":results[0][0],"avg":results[0][2],"max":results[0][2]}]
+    return jsonify(temp_data)
+
+# Display min,avg and max for date>=start and data<=end when the user hits 
+# the /api/v1.0/start/end/<start>/<end> route
+
+@app.route("/api/v1.0/start/end/<start>/<end>")
+def start_end_date_temp(start,end):
+    # Find min,avg and max for date>=start and date<=end
+    results=session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
+        filter(Measurement.date >=start).filter(Measurement.date <=end).all()
+    session.close()
+    # Create a list of dictionary from the results data
+    temp_data =[{"min":results[0][0],"avg":results[0][2],"max":results[0][2]}]
+    return jsonify(temp_data)
 
 ################################################# 
 # Define main behavior
 #################################################
 if __name__ == "__main__":
     app.run(debug=True)
+
+
